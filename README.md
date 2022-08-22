ef809f691589ee34942d1881b2aacc07f47adbff
Vlad Silagy <vladsilagy@gmail.com>
2022-08-22 19:26:47 -0400

    Added error message text and password event listener to verify if they match

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # **The Odin Project**
 
-## Full Stack JavaScript : Project [Sign-Up Form](https://www.theodinproject.com/lessons/node-path-intermediate-html-and-css-sign-up-form)
+## Full Stack JavaScript Project : [Sign-Up Form](https://www.theodinproject.com/lessons/node-path-intermediate-html-and-css-sign-up-form)
 
 [Live Preview](vsilagy.github.io/sign-up-form/)
---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('.form')
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password')
+const errorMessage = document.querySelector('.error-message')
+
+
+password.addEventListener('focusin', () => {
+  errorMessage.textContent = ""
+  password.classList.remove('error')
+  confirmPassword.classList.remove('error')
+})
+
+confirmPassword.addEventListener('focusout', () => {
+  if (confirmPassword.value !== "" && confirmPassword.value === password.value) {
+    errorMessage.textContent = ""
+    password.classList.remove('error')
+    confirmPassword.classList.remove('error')
+  } else if (confirmPassword.value !== password.value) {
+    errorMessage.textContent = "* Passwords do not match"
+    password.classList.add('error')
+    confirmPassword.classList.add('error')
+  }
+})
+
+
